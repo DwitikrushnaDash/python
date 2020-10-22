4f2f657d233a1ee5b0b03108489177cd39e87ceb
DwitikrushnaDash <71657533+DwitikrushnaDash@users.noreply.github.com>
2020-10-22 11:44:24 +0530

    Cryptography code

---
+++ CaeserCipher.py
@@ -0,0 +1,39 @@
+def generate_key(n):
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = {}
+    cnt = 0
+    for c in letters:
+        key[c] = letters[(cnt + n) % len(letters)]
+        cnt += 1
+    return key
+
+
+def get_decryption_key(key):
+    dkey = {}
+    for c in key:
+        dkey[key[c]] = c
+    return dkey
+
+
+def encrypt(key, message):
+    cipher = ""
+    for c in message:
+        if c in key:
+            cipher += key[c]
+        else:
+            cipher += c
+    return cipher
+
+
+# this is done by your enemy
+key = generate_key(3)
+message = "YOU ARE AWESOME"
+cipher = encrypt(key, message)
+
+# this is us breaking the cipher
+for i in range(26):
+    # insert code here
+    key = generate_key(i)
+    dkey = get_decryption_key(key)
+    dcrypt_msg = encrypt(dkey, cipher)
+    print(dcrypt_msg)
